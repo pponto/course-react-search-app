3be35c011bc26f68d8d7c8c93f963e9bac87bf12
pponto <pedromoreira@gmail.com>
2019-02-14 14:11:17 -0300

    Updates

---
+++ precache-manifest.93bd715c0625ef22afc5f2f7cf251497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6908df5e9370676ac637",
+    "url": "/course-react-search-app.git/static/js/runtime~main.6908df5e.js"
+  },
+  {
+    "revision": "ef5c1ed2b9315692692c",
+    "url": "/course-react-search-app.git/static/js/main.ef5c1ed2.chunk.js"
+  },
+  {
+    "revision": "88acd90a476c18b7c513",
+    "url": "/course-react-search-app.git/static/js/1.88acd90a.chunk.js"
+  },
+  {
+    "revision": "ef5c1ed2b9315692692c",
+    "url": "/course-react-search-app.git/static/css/main.5a17b37b.chunk.css"
+  },
+  {
+    "revision": "ba10de43e1937c2eb7cc35cd0d8da5b4",
+    "url": "/course-react-search-app.git/index.html"
+  }
+];
